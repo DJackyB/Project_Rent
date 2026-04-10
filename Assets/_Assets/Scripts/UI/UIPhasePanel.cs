5f8ef490bf2fb39563f7b161ac7c093f022561d1
DJackyB <166782214+DJackyB@users.noreply.github.com>
2026-04-10 17:50:59 +0800

    移除结束回合按钮的硬编码界面表现

--- Assets/_Assets/Scripts/UI/UIPhasePanel.cs
+++ Assets/_Assets/Scripts/UI/UIPhasePanel.cs
@@ -19,7 +19,6 @@ namespace BaoZuPo.UI
 
         private void OnEnable()
         {
-            EnsureRuntimeLayout();
             if (endTurnButton != null)
             {
                 endTurnButton.onClick.RemoveListener(OnEndTurnClicked);
@@ -37,8 +36,6 @@ namespace BaoZuPo.UI
 
         public void UpdatePhase(string phaseName)
         {
-            EnsureRuntimeLayout();
-
             if (phaseText != null)
             {
                 phaseText.text = GameText.PhaseName(phaseName);
@@ -62,67 +59,6 @@ namespace BaoZuPo.UI
             {
                 TurnManager.Instance.EndActionPhase();
             }
-        }
-
-        private void EnsureRuntimeLayout()
-        {
-            var rootCanvas = GetComponentInParent<Canvas>();
-            if (rootCanvas == null)
-            {
-                return;
-            }
-
-            if (endTurnButton == null)
-            {
-                endTurnButton = CreateRuntimeButton(rootCanvas.transform);
-                buttonText = endTurnButton.GetComponentInChildren<TextMeshProUGUI>();
-            }
-
-            endTurnButton.transform.SetParent(rootCanvas.transform, false);
-
-            var rect = endTurnButton.transform as RectTransform;
-            if (rect != null)
-            {
-                rect.anchorMin = new Vector2(1f, 0f);
-                rect.anchorMax = new Vector2(1f, 0f);
-                rect.pivot = new Vector2(1f, 0f);
-                rect.anchoredPosition = new Vector2(-24f, 24f);
-                rect.sizeDelta = new Vector2(180f, 56f);
-            }
-
-            var image = endTurnButton.GetComponent<Image>();
-            if (image != null)
-            {
-                image.color = new Color(0.16f, 0.35f, 0.19f, 0.94f);
-            }
-
-        }
-
-        private static Button CreateRuntimeButton(Transform parent)
-        {
-            var buttonObject = new GameObject("EndTurnButton", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image), typeof(Button));
-            buttonObject.transform.SetParent(parent, false);
-
-            var image = buttonObject.GetComponent<Image>();
-            image.sprite = UIRuntimeSpriteUtility.GetWhiteSprite();
-            image.type = Image.Type.Sliced;
-
-            var labelObject = new GameObject("Label", typeof(RectTransform), typeof(CanvasRenderer), typeof(TextMeshProUGUI));
-            labelObject.transform.SetParent(buttonObject.transform, false);
-
-            var labelRect = labelObject.GetComponent<RectTransform>();
-            labelRect.anchorMin = Vector2.zero;
-            labelRect.anchorMax = Vector2.one;
-            labelRect.offsetMin = Vector2.zero;
-            labelRect.offsetMax = Vector2.zero;
-
-            var label = labelObject.GetComponent<TextMeshProUGUI>();
-            label.fontSize = 24f;
-            label.alignment = TextAlignmentOptions.Center;
-            label.color = Color.white;
-            label.raycastTarget = false;
-
-            return buttonObject.GetComponent<Button>();
         }
     }
 }
